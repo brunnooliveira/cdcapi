19e0fc06bb78dde018dfc64de029fd65df3d60b0
Brunno Oliveira <brunnogoncalves@gmail.com>
2021-05-14 22:43:32 -0300

    feat(author): add author repository and disable jpa auto transaction

--- src/main/java/br/com/bgdo/cdcapi/bookdetail/CrudAuthorsController.java
+++ src/main/java/br/com/bgdo/cdcapi/bookdetail/CrudAuthorsController.java
@@ -1,26 +1,24 @@
 package br.com.bgdo.cdcapi.bookdetail;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CrudAuthorsController {
 
-  @PersistenceContext
-  private EntityManager manager;
+  @Autowired
+  private AuthorRepository authorRepository;
 
   @PostMapping("/api/author")
   @Transactional
   public void newAuthor(@Valid @RequestBody NewAuthorForm form) {
     Author newAuthor = form.newAuthor();
-    manager.persist(newAuthor);
+    authorRepository.save(newAuthor);
   }
 
 }
