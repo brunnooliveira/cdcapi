6daa5832aa7cc9ca727a510e39502887fe45593c
Brunno Oliveira <brunnogoncalves@gmail.com>
2021-05-16 17:47:48 -0300

    feat(cart): configure legacy tomcat cookie processor

--- src/main/java/br/com/bgdo/cdcapi/CdcapiApplication.java
+++ src/main/java/br/com/bgdo/cdcapi/CdcapiApplication.java
@@ -1,7 +1,11 @@
 package br.com.bgdo.cdcapi;
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -10,6 +14,12 @@ public class CdcapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CdcapiApplication.class, args);
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+		return (factory) -> factory
+				.addContextCustomizers((context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
 	}
 
 }
